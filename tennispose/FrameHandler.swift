b461ebf50d5501757054ea4f8aef394186d99144
Pradeep Banavara <pradeepbs@gmail.com>
2024-02-11 23:07:23 +0530

    Removed context and added processing to the (1,56,8300) to convert it into the required CoreML format.

--- tennispose/FrameHandler.swift
+++ tennispose/FrameHandler.swift
@@ -8,7 +8,15 @@
 import Foundation
 import AVFoundation
 import CoreImage
+import CoreML
 
+
+struct Prediction {
+    var labelIndex: Int
+    var confidence: Float
+    var boundingBox: CGRect
+    var points: NSMutableArray
+}
 
 class FrameHandler: NSObject, ObservableObject{
     @Published var frame: CGImage?
@@ -77,18 +85,16 @@ extension FrameHandler: AVCaptureVideoDataOutputSampleBufferDelegate {
     func imageFromSampleBuffer(sampleBuffer: CMSampleBuffer) -> CGImage? {
         guard let imageBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return nil }
         CVPixelBufferLockBaseAddress(imageBuffer, .readOnly)
-
+        var predictions: NSMutableArray = NSMutableArray()
+        
         let baseAddress = CVPixelBufferGetBaseAddress(imageBuffer)
         let pointer  = baseAddress!.assumingMemoryBound( to: UInt8.self )
         let height   = CVPixelBufferGetHeight(imageBuffer)
         let bytesPerRow = CVPixelBufferGetBytesPerRow(imageBuffer)
-        let pixBytes = height * bytesPerRow
         let cropWidth = 640
         let cropHeight = 640
         let colorSpace = CGColorSpaceCreateDeviceRGB()
         let options: NSDictionary = [:]
-
-        let context = CGContext(data: baseAddress, width: cropWidth, height: cropHeight, bitsPerComponent: 8, bytesPerRow: bytesPerRow, space: colorSpace, bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue | CGBitmapInfo.byteOrder32Little.rawValue)
         CVPixelBufferUnlockBaseAddress(imageBuffer, .readOnly)
         var targetBuffer: CVPixelBuffer?
         let results = CVPixelBufferCreateWithBytes(
@@ -103,14 +109,68 @@ extension FrameHandler: AVCaptureVideoDataOutputSampleBufferDelegate {
             options,
             &targetBuffer
         )
-        
+        NSLog("Pixel buffer resizing results", results)
         
         let model = try? yolov8n_pose()
         let output = try? model!.prediction(image: targetBuffer!)
         let features = output?.featureValue(for: "var_1035")
         let features_val = features?.multiArrayValue
         print (features_val?.shape)
+        let new_predictions = processFeaturesMultiArray(features: features_val!, predictions: predictions)
+        
         return nil
+    }
+    
+    func processFeaturesMultiArray(features: MLMultiArray, predictions:NSMutableArray) -> NSMutableArray{
+        /**
+         This function takes in a  MLMultiArray of shape ( 1, 56, 8500 ) and converts that array into keypoints for overlay on images
+         */
+        let gridWidth = features.shape[2].intValue
+        let gridHeight = features.shape[1].intValue
+        let keypointsNum = 17 // keypoints
+        let keypointsDim = 3 // Dimension
+        let classesNum = gridHeight - 4 - (keypointsNum * keypointsDim)
+        let threshold:Float = 0.25
+        let inputWidth:Float = 640.0
+        for j in stride(from: 0, to: gridWidth, by: 1) {
+            var classIndex = -1
+            var maxScore: Float = 0.0
+            for i in stride(from: 4, to:  4 + classesNum, by: 1){
+                let score = (features[(i * gridWidth) + j]).floatValue
+                if (score > maxScore) {
+                    classIndex = i - 4
+                    maxScore = score
+                }
+            }
+            if (maxScore > threshold) {
+                var x = features[(0 * gridWidth) + j].floatValue
+                var y = features[(1 * gridWidth) + j].floatValue
+                let w = features[(2 * gridWidth) + j].floatValue
+                let h = features[(3 * gridWidth) + j].floatValue
+                x -= w/2
+                y -= h/2
+                let rectWidth = x/inputWidth
+                let rectHeight = 1.0 - (y + h)/inputWidth
+                let rect = CGRectMake(CGFloat(rectWidth), CGFloat(rectHeight), CGFloat(w)/CGFloat(inputWidth), CGFloat(h)/CGFloat(inputWidth))
+                let pointsArray = NSMutableArray()
+                let visibleArray = NSMutableArray()
+                
+                for k in stride(from: 4 + classesNum, to: gridHeight, by: 3){
+                    var x = features[((k + 0) * gridWidth) + j]
+                    var y = features[((k + 1) * gridWidth) + j]
+                    var v = features[((k + 2) * gridWidth) + j]
+                    var point:CGPoint = CGPointMake(CGFloat(x) / CGFloat(inputWidth), 1.0 - CGFloat(y) / CGFloat(inputWidth))
+                    pointsArray.add(point)
+                }
+                var prediction:Prediction = Prediction(labelIndex: classIndex, confidence: maxScore, boundingBox: rect, points: pointsArray)
+                predictions.add(prediction)
+                
+            }
+            
+            
+        }
+        return predictions
+        
     }
     
 }
